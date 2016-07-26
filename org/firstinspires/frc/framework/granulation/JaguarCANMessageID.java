85dab278770134a3c7f33ce7576ae47bf625be61
YoshiRulz <yoshiisawsm@gmail.com>
2016-07-26 20:17:35 +1000

    Added the last codes from Jaguar's verify()

--- org/firstinspires/frc/framework/granulation/JaguarCANMessageID.java
+++ org/firstinspires/frc/framework/granulation/JaguarCANMessageID.java
@@ -2,7 +2,7 @@ package org.firstinspires.frc.framework.granulation;
 
 /**
  * @author FRC 4739 Thunderbolts Robotics
- * @version 2016-07-19/01
+ * @version 2016-07-26/00
  */
 enum JaguarCANMessageID {
 	NULL(0),
@@ -17,14 +17,14 @@ enum JaguarCANMessageID {
 	setVoltageRampRate_Percent(33685696), setVoltageRampRate_Voltage(33687808),
 	disableControl_Percent(33685568), disableControl_Current(33686592), disableControl_Speed(33688640), disableControl_Position(33689664), disableControl_Voltage(33687616),
 	setSpeedReference(33686912), setPositionReference(33688960),
-	configEncoderCodesPerRev(33692736), configNeutralMode(33692864), configPotentiometerTurns(33692800), configLimitMode(33692928), configForwardLimit(33692992), configReverseLimit(33693056), configMaxOutputVoltage(33693120);
+	configEncoderCodesPerRev(33692736), configNeutralMode(33692864), configPotentiometerTurns(33692800), configLimitMode(33692928), configForwardLimit(33692992), configReverseLimit(33693056), configMaxOutputVoltage(33693120),
+	setControlEnabled(33691200), setFaultTime(33693184),
+	getHardwareVersion(520225088);
 
 	private final int value;
-
 	JaguarCANMessageID(int i) {
 		value = i;
 	}
-
 	public int getValue() {
 		return value;
 	}
